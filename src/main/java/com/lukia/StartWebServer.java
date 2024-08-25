e804efd3fec193c500f22fcb6e13dbf229445cc8
bella <maekawatakaya10.8@gmail.com>
2024-08-26 02:26:08 +0900

    update

--- src/main/java/com/lukia/StartWebServer.java
+++ src/main/java/com/lukia/StartWebServer.java
@@ -3,5 +3,6 @@ package com.lukia;
 public class StartWebServer {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // aaaaaaaaaaaaaabbbbbbb
     }
 }
